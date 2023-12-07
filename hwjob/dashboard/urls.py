19888863d3f1b13adccc986f24da57da93570e3a
Florianlgv <florianlangevine37@gmail.com>
2023-12-07 14:17:28 +0100

    initial commit

---
+++ hwjob/dashboard/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from dashboard.views import consumption_view, search_client_view
+
+app_name = "dashboard"
+urlpatterns = [
+    path("", search_client_view, name="search_client"),
+    path(
+        f"consumption/<int:client_id>",
+        consumption_view,
+        name="consumption_details",
+    ),
+]
