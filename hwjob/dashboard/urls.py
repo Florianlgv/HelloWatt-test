2cf43987f97efaa256e3cdb4cf61c2a3831f8cee
Florianlgv <florianlangevine37@gmail.com>
2023-12-08 17:26:25 +0100

    api REST endpoint

--- hwjob/dashboard/urls.py
+++ hwjob/dashboard/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from dashboard.views import consumption_view, search_client_view
+from dashboard.views import (
+    consumption_view,
+    ClientConsumptionDetailView,
+)
 
 app_name = "dashboard"
 urlpatterns = [
-    path("", search_client_view, name="search_client"),
-    path(
-        f"consumption/<int:client_id>",
-        consumption_view,
-        name="consumption_details",
-    ),
+    path("conso/<int:client_id>/", ClientConsumptionDetailView.as_view()),
 ]
